d8b6e87b20e8bdb005291f309ffe45a6543fc1c0
jandaio <amoggia@gmail.com>
2022-08-14 17:12:20 -0600

    mejoras de boostrap. Carrusel ahora mas usuable

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+
+
+window.addEventListener("load", (e) => {
+    var myCarousel = document.getElementById("carouselhuay")
+    var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: 4000,
+        wrap: true,
+        touch: true,
+        pause: 'hover',
+        ride: 'carousel'
+    })
+
+    carousel.cycle();
+
+    myCarousel.addEventListener("click", () => {
+        carousel.next();
+    })
+
+
+})
