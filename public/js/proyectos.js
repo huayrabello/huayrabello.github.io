cbc04b4fb7de5c6fa3556d0d9bb0957a02bc576c
jandaio <amoggia@gmail.com>
2022-08-14 22:25:30 -0600

    mejoras proyectos

---
+++ public/js/proyectos.js
@@ -0,0 +1,40 @@
+
+window.addEventListener("load", (e) => {
+
+    var myCarousel = document.getElementById("carouselhuay")
+    var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: 4000,
+        wrap: true,
+        touch: true,
+        pause: 'hover',
+        ride: 'carousel'
+    })
+
+    carousel.cycle();
+
+    myCarousel.addEventListener("click", () => {
+        carousel.next();
+    })
+
+
+
+    var divmodal = document.getElementById('proyectoModal')
+    const myModal = new bootstrap.Modal(divmodal, { keyboard: true, backdrop: true })
+
+    var imagesproyecto = document.getElementsByClassName("imgproyect");
+
+    for (let index = 0; index < imagesproyecto.length; index++) {
+        const element = imagesproyecto[index];
+        element.addEventListener("click", () => {
+            carousel.to(index);
+            myModal.toggle();
+        })
+    }
+
+
+    var divmodalbody = document.getElementById('modalbody')
+
+    divmodal.addEventListener("click", () => { myModal.hide() })
+
+    divmodalbody.addEventListener("click", (e) => { e.stopPropagation() })
+})
