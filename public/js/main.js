525765154ba55e7868cdee632a559690604d73e3
jandaio <amoggia@gmail.com>
2022-08-20 20:20:13 -0600

    Flechas en carruseles

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,10 @@
 
 window.addEventListener("load", (e) => {
     var myCarousel = document.getElementById("carouselhuay")
+
+    var carPrev = document.getElementById("carousel_prev")
+    var carNext = document.getElementById("carousel_next")
+
     var carousel = new bootstrap.Carousel(myCarousel, {
         interval: 2000,
         wrap: true,
@@ -12,11 +16,20 @@ window.addEventListener("load", (e) => {
 
     carousel.cycle();
 
-    myCarousel.addEventListener("click", (e) => {
-        let halfCarouselWidth = myCarousel.clientWidth / 2
-        console.log(halfCarouselWidth + " - " + e.offsetX)
-        if (e.offsetX < halfCarouselWidth) carousel.prev();
-        else carousel.next()
+    // myCarousel.addEventListener("click", (e) => {
+    //     let halfCarouselWidth = myCarousel.clientWidth / 2
+    //     console.log(halfCarouselWidth + " - " + e.offsetX)
+    //     if (e.offsetX < halfCarouselWidth) carousel.prev();
+    //     else carousel.next()
+    // })
+
+    carPrev.addEventListener("click", (e) => {
+        carousel.prev();
+
+    })
+
+    carNext.addEventListener("click", (e) => {
+        carousel.next()
     })
 
 
