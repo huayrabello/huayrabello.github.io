92acef9da22d940da7740ca607ef7ff8297fdb5b
jandaio <amoggia@gmail.com>
2022-08-29 13:01:13 -0600

    Colaboraciones ahora para mobiles - responsive !

--- public/js/colaboraciones.js
+++ public/js/colaboraciones.js
@@ -1,36 +1,49 @@
 
 
+function addHorizontalSlider(mediaQuery) {
 
+
+    if (mediaQuery.matches) { // If media query matches
+        $('.inner_slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 1,
+            centerMode: false,
+            variableWidth: true,
+            appendArrows: false,
+            appendDots: false,
+            centerMode: false,
+            draggable: true,
+            swipeToSlide: false,
+            responsive: true,
+
+        });
+
+        //Carrusel
+        var prev = document.getElementById("slider_prev");
+        var next = document.getElementById("slider_next");
+
+        prev.addEventListener("click", (e) => {
+            $('.inner_slider').slick('slickPrev');
+
+        })
+
+        next.addEventListener("click", (e) => {
+            $('.inner_slider').slick('slickNext');
+        })
+    } else {
+
+        $('.inner_slider').slick('unslick');
+    }
+}
 
 
 window.addEventListener("load", (e) => {
-    $('.inner_slider').slick({
-        dots: false,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        centerMode: false,
-        variableWidth: true,
-        appendArrows: false,
-        appendDots: false,
-        centerMode: false,
-        draggable: true,
-        swipeToSlide: false,
-        responsive: true
-    });
 
-    //Carrusel
-    var prev = document.getElementById("slider_prev");
-    var next = document.getElementById("slider_next");
-
-    prev.addEventListener("click", (e) => {
-        $('.inner_slider').slick('slickPrev');
-
-    })
-
-    next.addEventListener("click", (e) => {
-        $('.inner_slider').slick('slickNext');
-    })
+    const mediaQuery = window.matchMedia("(min-width: 992px)")
+    addHorizontalSlider(mediaQuery) // Call listener function at run time
+    mediaQuery.addEventListener("change", () => { addHorizontalSlider(mediaQuery); })
 
 })
 
