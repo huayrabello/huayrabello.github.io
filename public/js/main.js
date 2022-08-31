1a6df69c2cf4addf69e165f15159696430560f9b
jandaio <amoggia@gmail.com>
2022-08-30 18:44:22 -0600

    nuevo menu en todos lados

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,26 @@
 
-
 window.addEventListener("load", (e) => {
-    // var myCarousel = document.getElementById("carouselhuay")
-
-    // var carousel = new bootstrap.Carousel(myCarousel,
-    //     //     {
-    //     //     interval: 2000,
-    //     //     wrap: true,
-    //     //     touch: true,
-    //     //     pause: 'hover',
-    //     //     ride: 'carousel'
-    //     // }
-    // )
-
-    // carousel.cycle();
-
-    // myCarousel.addEventListener("click", (e) => {
-    //     let halfCarouselWidth = myCarousel.clientWidth / 2
-    //     console.log(halfCarouselWidth + " - " + e.offsetX)
-    //     if (e.offsetX < halfCarouselWidth) carousel.prev();
-    //     else carousel.next()
-    // })
 
     const closemenu = document.getElementById("closemenu")
+    const openmenu = document.getElementById("openmenu")
     const menu = document.getElementById("mainmenu")
+    const mmbody = document.getElementById("mm-body")
+
+    openmenu.addEventListener("click", () => {
+        mmbody.classList.remove('d-none')
+        mmbody.style.height = ' 100vh'
+        closemenu.classList.remove('d-none')
+        openmenu.classList.add('d-none')
+    }
+    )
+
 
     closemenu.addEventListener("click", () => {
+        mmbody.style.height = 'auto'
+        closemenu.classList.add('d-none')
+        openmenu.classList.remove('d-none')
+        mmbody.classList.add('d-none')
 
-        menu.classList.add('d-none')
     })
 
 
