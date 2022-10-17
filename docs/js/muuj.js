933453b52466db9805ba7cf792a5b22794453668
jandaio <amoggia@gmail.com>
2022-10-16 23:38:47 -0600

    muuj falta menos

---
+++ docs/js/muuj.js
@@ -0,0 +1,81 @@
+
+function addHorizontalSlider(id, mediaQuery) {
+
+    if (mediaQuery.matches) { // If media query matches
+        $(id).slick({
+            dots: false,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            centerMode: false,
+            variableWidth: true,
+            appendArrows: false,
+            appendDots: false,
+            draggable: true,
+            swipeToSlide: false,
+            responsive: false
+        });
+
+        //Carrusel
+        var prev = document.getElementById("slider_prev");
+        var next = document.getElementById("slider_next");
+
+        prev.addEventListener("click", (e) => {
+            $(id).slick('slickPrev');
+            $("#slidermainimage").attr("src", $(".slick-current img").attr("src"))
+        })
+
+        next.addEventListener("click", (e) => {
+            $(id).slick('slickNext');
+            $("#slidermainimage").attr("src", $(".slick-current img").attr("src"))
+        })
+    } else {
+        try {
+            $(id).slick('unslick');
+        } catch (error) {
+            console.log("no existe instancia de slick..,")
+        }
+
+    }
+}
+
+window.addEventListener("DOMContentLoaded", (e) => {
+
+    const mediaQuery = window.matchMedia("(min-width: 992px)")
+
+    addHorizontalSlider("#full_slider", mediaQuery)
+
+    mediaQuery.addEventListener("change", () => {
+
+        addHorizontalSlider("#full_slider", mediaQuery);
+
+    })
+
+})
+
+window.addEventListener("load", (e) => {
+    const imageSwapSpeed = 2 // segundos
+    const divimgs = [document.getElementById("swapimg1"), document.getElementById("swapimg2")];
+
+
+    setInterval(() => {
+        if (divimgs[0].style.display == "none") {
+            divimgs[1].style.display = "none";
+            divimgs[0].style.display = "block";
+
+        }
+        else {
+            divimgs[0].style.display = "none";
+            divimgs[1].style.display = "block";
+        }
+
+
+    }, imageSwapSpeed * 1000)
+
+
+    $(".slide").on("click", function (element) {
+        $("#slidermainimage").attr("src", $(element.target).attr("src"));
+    })
+})
+
